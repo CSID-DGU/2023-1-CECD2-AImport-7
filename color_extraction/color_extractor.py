9dd54d17714e05639b06a67ea9d27119ce3a3d7f
강동희 <kangdonghee@gangdonghuiui-MacBookPro.local>
2023-10-14 20:06:35 +0900

    extractor_utils.py edit

--- color_extraction/color_extractor.py
+++ color_extraction/color_extractor.py
@@ -1,54 +1,26 @@
 import cv2
 import numpy as np
+import color_table
 
-# 이미지 파일 경로
-image_path = "./test.jpeg"
-# 결과 행렬을 저장할 파일 경로
-output_path = "final_result1.txt"
+def color_extractor(segImg):
 
-# 컬러 매칭 테이블 정의 (각 컬러와 RGB 값)
-color_table = {
-    'black': (0, 0, 0),
-    'white': (255, 255, 255),
-    'red': (255, 0, 0),
-    'orange': (255, 165, 0),
-    'green': (0, 128, 0),
-    'blue': (0, 0, 255),
-    'gray': (128, 128, 128),
-    'purple': (128, 0, 128),
-    'sky_blue': (0, 191, 255)
-}
+    image = cv2.imread(segImg)
 
-# 이미지를 엽니다.
-image = cv2.imread(image_path)
 
-# 이미지 크기 가져오기
-height, width, _ = image.shape
+    #segImg의 크기를 상대크기로 줄이기
+    dst = cv2.resize(image, dsize=(0,0), fx=0.05, fy=0.05,  interpolation=cv2.INTER_AREA)
 
-# 그레이스케일로 변환
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# 엣지 검출 (Canny 알고리즘 사용)
-edges = cv2.Canny(gray, threshold1=30, threshold2=100)
+    #resize한 이미지의 크기에 맞는 빈 행렬 생성
+    result_matrix = np.full((dst.shape[0], dst.shape[1]), 0, dtype=object)
 
-# 객체 내부의 픽셀에 대한 마스크 생성
-object_mask = np.zeros_like(edges)
-object_mask[edges != 0] = 1
-
-# 결과 행렬 초기화
-result_matrix = np.full((height, width), None, dtype=object)
-
-# 객체 내부 색상 추출 및 매칭
-for y in range(height):
-    for x in range(width):
-        if object_mask[y, x] == 1:
-            pixel = image[y, x]
-            closest_color = min(color_table, key=lambda color: np.linalg.norm(np.array(color_table[color]) - np.array(pixel)))
+    #dst의 이미지의 색상을 추출하여 color name을 result_matrix에 저장
+    for y in range(dst.shape[0]):
+        for x in range(dst.shape[1]):
+            pixel = dst[y, x]
+            closest_color = min(color_table.color_table, key=lambda color: np.linalg.norm(np.array(color_table.color_table[color]) - np.array(pixel)))
             result_matrix[y, x] = closest_color
 
-# 결과 행렬을 텍스트 파일로 저장
-with open(output_path, 'w') as file:
-    for row in result_matrix:
-        file.write(' '.join(map(str, row)) + '\n')
+    # Numpy 형식의 color matrix 반환
+    return result_matrix
 
-print("Done.")
--- color_extraction/color_extractor_utils.py
+++ color_extraction/color_extractor_utils.py
@@ -4,32 +4,22 @@ import color_table
 
 def color_extractor(segImg):
 
-    # 이미지 크기 가져오기
-    height, width, _ = segImg.shape
+    image = cv2.imread(segImg)
 
-    # RGB에서 BGR로 변환 (이미지 분할된 원본 이미지)
-    segCV = cv2.cvtColor(segImg, cv2.COLOR_RGB2BGR)
 
-    # 그레이스케일로 변환
-    gray = cv2.cvtColor(segImg, cv2.COLOR_BGR2GRAY)
+    #segImg의 크기를 상대크기로 줄이기
+    dst = cv2.resize(image, dsize=(0,0), fx=0.05, fy=0.05,  interpolation=cv2.INTER_AREA)
 
-    # 엣지 검출 (Canny 알고리즘 사용)
-    edges = cv2.Canny(gray, threshold1=30, threshold2=100)
 
-    # 객체 내부의 픽셀에 대한 마스크 생성
-    object_mask = np.zeros_like(edges)
-    object_mask[edges != 0] = 1
+    #resize한 이미지의 크기에 맞는 빈 행렬 생성
+    result_matrix = np.full((dst.shape[0], dst.shape[1]), 0, dtype=object)
 
-    # 결과 행렬 초기화
-    result_matrix = np.full((height, width), None, dtype=object)
+    #dst의 이미지의 색상을 추출하여 color name을 result_matrix에 저장
+    for y in range(dst.shape[0]):
+        for x in range(dst.shape[1]):
+            pixel = dst[y, x]
+            closest_color = min(color_table.color_table, key=lambda color: np.linalg.norm(np.array(color_table.color_table[color]) - np.array(pixel)))
+            result_matrix[y, x] = closest_color
 
-    # 객체 내부 색상 추출 및 매칭
-    for y in range(height):
-        for x in range(width):
-            if object_mask[y, x] == 1:
-                pixel = segCV[y, x]
-                closest_color = min(color_table, key=lambda color: np.linalg.norm(np.array(color_table[color]) - np.array(pixel)))
-                result_matrix[y, x] = closest_color
-    
     # Numpy 형식의 color matrix 반환
     return result_matrix
